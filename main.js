b3c2dc8146486a58aacb6b7d1de215c6adabab66
nikolg0 <nikig0731@gmail.com>
2024-04-16 14:55:43 +0200

    poprawa

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-let li = "56";
-let cz = "73";
-let ba = "26";
-let array = [56, 73, 26];
+let li = 56;
+let cz = 73;
+let ba = 26;
+let array = [li, cz, ba];
 console.log(li);
 console.log(cz);
 console.log(ba);
